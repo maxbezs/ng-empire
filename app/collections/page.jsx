11ca4d43a6f21942c697dbd0144e8a542d4e4b81
maxbezs <baks.bezsmertnuy@gmail.com>
2024-10-21 16:25:03 +0200

    1

---
+++ app/collections/page.jsx
@@ -0,0 +1,14 @@
+const page = () => {
+  return (
+    <div class="grid grid-cols-3 gap-4">
+      <div class="bg-blue-200 p-4">Column 1</div>
+      <div class="bg-blue-200 p-4">Column 2</div>
+      <div class="bg-blue-200 p-4">Column 3</div>
+      <div class="bg-blue-200 p-4">Column 4</div>
+      <div class="bg-blue-200 p-4">Column 5</div>
+      <div class="bg-blue-200 p-4">Column 6</div>
+    </div>
+  );
+};
+
+export default page;
